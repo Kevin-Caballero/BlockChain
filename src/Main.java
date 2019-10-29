9bdf38a4896fd6b018cfe773a1efddb2d075729e
Kevin <50716473+Kevin-Caballero@users.noreply.github.com>
2019-10-29 17:08:24 +0100

    iteration fixed

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         kvnCoin.addBlock("primer bloque");
         kvnCoin.addBlock("segundo bloque");
 
-        for (int i = 0; i < kvnCoin.getSize()-1; i++) {
+        for (int i = 0; i < kvnCoin.getSize(); i++) {
             System.out.println("Index: "+kvnCoin.getItem(i).index);
             System.out.println("Date: "+kvnCoin.getItem(i).date);
             System.out.println("Data: "+kvnCoin.getItem(i).data);
